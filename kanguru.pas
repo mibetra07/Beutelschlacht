688507a665524753ca0ae32daafc70ed379872c6
mibetra07 <158103827+mibetra07@users.noreply.github.com>
2024-12-09 12:34:11 +0100

    Add files via upload

--- kanguru.pas
+++ kanguru.pas
@@ -5,9 +5,48 @@ unit kanguru;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls, Path;
+
+type TKanguru = class
+  public
+    x, y, width, height : integer;
+    bild: Timage;
+    attackspeed, damage, range : integer;
+    cancamo : boolean;
+
+    constructor create();
+    procedure attack();
+  private
+
+  end;
+
+  TBogenkanguru = class(TKanguru)
+
+  end;
+
+  TZauberkanguru = class(Tkanguru)
+
+  end;
+
+  TNinjakanguru = class(Tkanguru)
+
+  end;
+
+  TEiskanguru = class(Tkanguru)
+
+  end;
 
 implementation
+  constructor Tkanguru.create();
+  begin
+    inherited create();
+
+  end;
+
+  procedure Tkanguru.attack();
+  begin
+
+  end;
 
 end.
 
