f42d10781cdb5f34761b214954fc2ab5d1dcd374
BebbetGrache <115918995+BebbetGrache@users.noreply.github.com>
2024-12-24 21:54:23 +0100

    Add files via upload

--- wave.pas
+++ wave.pas
@@ -4,7 +4,7 @@ unit Wave;
 
 interface
 Uses Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls, Pinguin;
-procedure tick(normal, helm, schild, boss, tarn, map: integer; Pinguin: TPinguin);
+procedure tick(map: integer; Pinguin: TPinguin);
 type
   TWave  = class
       public
@@ -21,82 +21,69 @@ begin
   begin
        for i := 1 to normal do
            Form5.Pinguin[i] := TPinguin.Create(1, random(offsetmultiplier) * 50);
-       {for i := 1 to helm do
-           Form5.Pinguin[i] := THelmPinguin.Create(1);
-       for i := 1 to schild do
-           Form5.Pinguin[i] := TSchildPinguin.Create(1);
-       for i := 1 to boss do
-           Form5.Pinguin[i] := TBossPinguin.Create(1);
-       for i := 1 to tarn do
-           Form5.Pinguin[i] := TTarnPinguin.Create(1);}
+      // for i := 1 to helm do
+          // Form5.helmPinguin[i] := THelmPinguin.Create(1, random(offsetmultiplier) * 50 + 100);
   end
   else if map = 2 then
   begin
        for i := 1 to normal do
            Form6.Pinguin[i] := TPinguin.Create(2, random(offsetmultiplier) * 50);
-       {for i := 1 to helm do
-           Form6.Pinguin[i] := THelmPinguin.Create(2);
-       for i := 1 to schild do
-           Form6.Pinguin[i] := TSchildPinguin.Create(2);
-       for i := 1 to boss do
-           Form6.Pinguin[i] := TBossPinguin.Create(2);
-       for i := 1 to tarn do
-           Form6.Pinguin[i] := TTarnPinguin.Create(2);}
-  end;
-  if map = 1 then
-  begin
-       for i := 1 to helm do
-           Form5.helmPinguin[i] := THelmPinguin.Create(1, random(offsetmultiplier) * 50 + 100);
-       {for i := 1 to helm do
-           Form5.Pinguin[i] := THelmPinguin.Create(1);
-       for i := 1 to schild do
-           Form5.Pinguin[i] := TSchildPinguin.Create(1);
-       for i := 1 to boss do
-           Form5.Pinguin[i] := TBossPinguin.Create(1);
-       for i := 1 to tarn do
-           Form5.Pinguin[i] := TTarnPinguin.Create(1);}
-  end
-  else if map = 2 then
-  begin
-       for i := 1 to helm do
-           Form6.helmPinguin[i] := THelmPinguin.Create(2, random(offsetmultiplier) * 50 + 100);
-       {for i := 1 to helm do
-           Form6.Pinguin[i] := THelmPinguin.Create(2);
-       for i := 1 to schild do
-           Form6.Pinguin[i] := TSchildPinguin.Create(2);
-       for i := 1 to boss do
-           Form6.Pinguin[i] := TBossPinguin.Create(2);
-       for i := 1 to tarn do
-           Form6.Pinguin[i] := TTarnPinguin.Create(2);}
-  end;
+       //for i := 1 to helm do
+        //   Form6.helmPinguin[i] := THelmPinguin.Create(2, random(offsetmultiplier) * 50 + 100);
+       end;
 end;
-procedure tick(normal, helm, schild, boss, tarn, map: integer; Pinguin: TPinguin);
+
+procedure tick(map: integer; Pinguin: TPinguin);
 var i, j, speed: integer;
 begin
+  Pinguin.laufen(1);
+  if Pinguin.hp <= 0 then
+     Pinguin.currentPath := 100;
   if map = 1 then
   begin
-
-  for i := 1 to normal do
-      begin
+      if Pinguin <> nil then
       Pinguin.laufen(1);
+      for j := 1 to 50 do
+          begin
+      if Form5.BogenKanguru[j] <> nil then
+      begin
        if (Form5.ticksPassed mod 20) = 0 then
         begin
-      Pinguin.attackcheck(1);
+      Form5.BogenKanguru[j].attack(1, Pinguin, 'Bogen');
+      if Form5.ticksPassed mod 40 = 0 then
+      Form5.BogenKanguru[1].bild.picture.LoadFromFile('images\Bogenguru.png');
+      end;
+      end;
+       if Form5.Kanguru[j] <> nil then
+      begin
+       if (Form5.ticksPassed mod 20) = 0 then
+        begin
+      Form5.Kanguru[j].attack(1, Pinguin, 'Boxer');
       if Form5.ticksPassed mod 40 = 0 then
       Form5.Kanguru[1].bild.picture.LoadFromFile('images\Boxerkanguru.png');
       end;
-  end;
-  for i := 1 to helm do
-      //Form5.HelmPinguin[i].laufen(1);
+      end;
+       if Form5.EisKanguru[j] <> nil then
+      begin
+       if (Form5.ticksPassed mod 20) = 0 then
+        begin
+      Form5.EisKanguru[j].attack(1, Pinguin, 'Eis');
+      if Form5.ticksPassed mod 40 = 0 then
+      Form5.EisKanguru[1].bild.picture.LoadFromFile('images\Eisguru.png');
+      if Form5.ticksPassed - Form5.slowedTick > 20 then
+       begin
+        Pinguin.slowed := false;
+        Pinguin.speed := 2;
+       end;
+
+
+      end;
+      end;
+
+      end;
   end
   else if map = 2 then
-  begin
-  for i := 1 to normal do
       Pinguin.laufen(2);
-  for i := 1 to helm do
-      Form6.HelmPinguin[i].laufen(2);
-  end;
-
 end;
 
 end.
